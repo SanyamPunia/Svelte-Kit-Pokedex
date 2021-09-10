b789d713da2d77534bd4e84d09d07efc1d94a314
SanyamPunia <lewarends@gmail.com>
2021-09-10 18:20:34 +0530

    add files

---
+++ src/routes/about.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+    <title>About</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<h2 class="text-center text-2xl">This Pokedex is made using <a class="addons" href="https://kit.svelte.dev/">Svelte Kit</a> and <a class="addons" href="https://pokeapi.co/">PokeAPI</a>!</h2>
+
+<style>
+    .addons {
+        color: #EB92BE;
+        transition: 0.3s;
+    }
+    .addons:hover {
+        color: darkviolet
+    }
+</style>
